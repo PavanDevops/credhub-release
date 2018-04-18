5e25f2f8b15355730879aaf062326c7d494455f6
Guillermo Kardolus <gkardolus@pivotal.io>
2018-04-18 17:40:24 -0400

    Update backup and restore scripts to enable/disable read_only_mode for credhub
    [#156505422] CredHub continues to serve "info," "version," and "interpolate" requests during a BBR backup
    
    Signed-off-by: Morolayo Lambo <mlambo@pivotal.io>

--- jobs/credhub/templates/post-restore-unlock.sh
+++ jobs/credhub/templates/post-restore-unlock.sh
@@ -3,6 +3,5 @@
 set -u
 
 export PATH=/var/vcap/bosh/bin:/var/vcap/jobs/credhub/bin:$PATH
-
-monit restart credhub
+curl "$(credhub api)/management" -X POST -d '{"read_only_mode":"false"}' -H "Authorization: $(credhub --token)" -H 'content-type: application/json' -k
 exec /var/vcap/jobs/credhub/bin/bbr/post-bbr-start
--- jobs/credhub/templates/pre-restore-lock.sh
+++ jobs/credhub/templates/pre-restore-lock.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 export PATH=/var/vcap/bosh/bin:$PATH
-monit stop credhub
+curl "$(credhub api)/management" -X POST -d '{"read_only_mode":"true"}' -H "Authorization: $(credhub --token)" -H 'content-type: application/json' -k
 exec /var/vcap/jobs/credhub/bin/bbr/wait-for-stop
